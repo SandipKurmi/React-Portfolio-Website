f025795727c940e4366c289f7853ddbf9791ef37
xsquare-110 <sandipkurmi0@gmail.com>
2022-05-24 19:03:47 +0530

    first commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from 'react'
+import './intro.scss'
+import { init } from 'ityped'
+
+
+const Intro = () => {
+    const textRef = useRef()
+
+    useEffect(() => {
+        init(textRef.current, { showCursor: false, strings: ['Developer', 'Designer', 'Content Creator'] })
+    }, [])
+
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Sandip Kurmi</h1>
+                    <h3>JavaScript <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Intro
